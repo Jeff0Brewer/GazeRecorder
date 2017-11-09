6709030eb9c15d6dc95a363a375941627ffe319b
Jeff0Brewer <jeffreybrewer2019@u.northwestern.edu>
2017-11-09 12:42:21 -0600

    v1
    initial

---
+++ Recorder/Recorder/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using EyeXFramework.Wpf;
+
+namespace Recorder
+{
+    public partial class App : Application
+    {
+        private WpfEyeXHost _eyeXHost;
+        public App()
+        {
+            _eyeXHost = new WpfEyeXHost();
+            _eyeXHost.Start();
+        }
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+            _eyeXHost.Dispose();
+        }
+    }
+}
---
+++ Recorder/Recorder/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows;
+using System.IO;
+using Tobii.EyeX.Framework;
+using EyeXFramework;
+
+namespace Recorder
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        String path = "Recordings/";
+        String name = "r";
+        String ext = ".csv";
+        String delim = ",";
+        TextWriter tw;
+        EyeXHost host;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            int num = 0;
+            String currPath = path + name + num.ToString() + ext;
+            while (File.Exists(currPath))
+                currPath = path + name + (num++).ToString() + ext;
+            tw = new StreamWriter(currPath);
+
+            host = new EyeXHost();
+            host.Start();
+            var gazeData = host.CreateGazePointDataStream(GazePointDataMode.LightlyFiltered);
+            gazeData.Next += newPoint;
+        }
+
+        private void newPoint(object s, EyeXFramework.GazePointEventArgs e)
+        {
+            tw.WriteLine(e.X.ToString() + delim + e.Y.ToString());
+        }
+
+        private void onExit(object s, EventArgs e) {
+            tw.Close();
+            host.Dispose();
+        }
+    }
+}
